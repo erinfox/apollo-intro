9d6c8c350b3a7a643076c416e1cab4cdd4a5ceab
Erin Fox <erinkayfox@gmail.com>
2019-01-29 13:31:39 -0500

    add button mutations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { gql } from 'apollo-boost';
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 
 const ALL_LIFTS_QUERY = gql`
   query {
     allLifts {
+      name
+      id
+      status
+      capacity
+      trailAccess {
+        id
+        name
+      }
+    }
+  }
+`;
+
+const LIFT_STATUS_MUTATION = gql`
+  mutation($id: ID!, $status: LiftStatus!) {
+    setLiftStatus(id: $id, status: $status) {
       id
       name
       status
-      capacity
     }
   }
 `;
@@ -21,8 +35,55 @@ const App = () => (
         <section>
           {!loading &&
             data.allLifts.map(lift => (
-              <div>
-                <h3>{lift.name}</h3>
+              <div key={lift.id}>
+                <h3>
+                  {lift.name} {lift.status}
+                </h3>
+                <Mutation mutation={LIFT_STATUS_MUTATION}>
+                  {changeStatus => (
+                    <button
+                      onClick={() =>
+                        changeStatus({
+                          variables: { id: lift.id, status: 'OPEN' },
+                        })
+                      }
+                    >
+                      OPEN
+                    </button>
+                  )}
+                </Mutation>
+                <Mutation mutation={LIFT_STATUS_MUTATION}>
+                  {changeStatus => (
+                    <button
+                      onClick={() =>
+                        changeStatus({
+                          variables: { id: lift.id, status: 'CLOSED' },
+                        })
+                      }
+                    >
+                      CLOSED
+                    </button>
+                  )}
+                </Mutation>
+                <Mutation mutation={LIFT_STATUS_MUTATION}>
+                  {changeStatus => (
+                    <button
+                      onClick={() =>
+                        changeStatus({
+                          variables: { id: lift.id, status: 'HOLD' },
+                        })
+                      }
+                    >
+                      HOLD
+                    </button>
+                  )}
+                </Mutation>
+                <h4>Trails</h4>
+                <ul>
+                  {lift.trailAccess.map(trail => (
+                    <li key={trail.id}> {trail.name}</li>
+                  ))}
+                </ul>
               </div>
             ))}
         </section>
